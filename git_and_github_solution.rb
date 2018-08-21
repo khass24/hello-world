226f5e143e2582791b70798ffff878e0ab38c0eb
kyle-hassett <42388066+kyle-hassett@users.noreply.github.com>
2018-08-21 13:32:38 -0500

    Add files via upload

---
+++ git_and_github_solution.rb
@@ -0,0 +1,7 @@
+# Paste a link to your github repository below:
+
+# https://github.com/kyle-hassett/hello-world
+
+# What questions do you have about what we've learned so far using git and github?:
+
+# I'm still a bit shaky on working with git, and I'm not sure if I was supposed to clone this file from the remote location on GitHub and then open it through the command line, but I guess I'll find out. The process of using git init, add, commit, log, status, etc. has definitely become more comfortable in the past few days, but I still feel it'll be awhile before I'm comfortable. 
